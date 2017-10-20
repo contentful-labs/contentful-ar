dbc7bf69060831841bc6144881aadc0779648944
Mario Bodemann <mario@contentful.com>
2017-10-20 16:41:16 +0200

    polish: Extract rendering from main activity

--- samples/java_arcore_hello_ar/app/src/main/java/com/google/ar/core/examples/java/helloar/HelloArActivity.java
+++ samples/java_arcore_hello_ar/app/src/main/java/com/google/ar/core/examples/java/helloar/HelloArActivity.java
@@ -16,12 +16,10 @@
 
 package com.google.ar.core.examples.java.helloar;
 
-import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -31,32 +29,18 @@ import com.google.ar.core.Config;
 import com.google.ar.core.Frame;
 import com.google.ar.core.Frame.TrackingState;
 import com.google.ar.core.HitResult;
-import com.google.ar.core.Plane;
 import com.google.ar.core.PlaneHitResult;
 import com.google.ar.core.Session;
-import com.google.ar.core.examples.java.helloar.rendering.CameraFeedRenderer;
-import com.google.ar.core.examples.java.helloar.rendering.ObjectRenderer;
-import com.google.ar.core.examples.java.helloar.rendering.ObjectRenderer.BlendMode;
-import com.google.ar.core.examples.java.helloar.rendering.PlaneAttachment;
-import com.google.ar.core.examples.java.helloar.rendering.PlaneRenderer;
-import com.google.ar.core.examples.java.helloar.rendering.PointCloudRenderer;
-import com.google.ar.core.exceptions.CameraException;
-import com.google.ar.core.exceptions.NotTrackingException;
+import com.google.ar.core.examples.java.helloar.rendering.Scene;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.concurrent.ArrayBlockingQueue;
-
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
 
 /**
  * This is a simple example that shows how to create an augmented reality (AR) application using
  * the ARCore API. The application will display any detected planes and will allow the user to
  * tap on a plane to place a 3d model of the Android robot.
  */
-public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.Renderer {
+public class HelloArActivity extends AppCompatActivity {
     private static final String TAG = HelloArActivity.class.getSimpleName();
 
     // Rendering. The Renderers are created here, and initialized when the GL surface is created.
@@ -72,30 +56,32 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
         }
     };
 
+    private final Scene.DrawingCallback drawCallback = new Scene.DrawingCallback() {
+        @Override public void onDraw(Frame frame) {
+            handleTap(frame);
+        }
+
+        @Override public void trackingPlane() {
+            hideLoadingMessage();
+        }
+    };
+
+    private Scene scene;
     private Config mDefaultConfig;
     private Session mSession;
-    private CameraFeedRenderer mCameraFeedRenderer = new CameraFeedRenderer();
     private Snackbar mLoadingMessageSnackbar = null;
-
-    private ObjectRenderer mVirtualObject = new ObjectRenderer();
-    private ObjectRenderer mVirtualObjectShadow = new ObjectRenderer();
-    private PlaneRenderer mPlaneRenderer = new PlaneRenderer();
-    private PointCloudRenderer mPointCloud = new PointCloudRenderer();
-
-    // Temporary matrix allocated here to reduce number of allocations for each frame.
-    private final float[] mAnchorMatrix = new float[16];
 
     // Tap handling and UI.
     private ArrayBlockingQueue<MotionEvent> mQueuedSingleTaps = new ArrayBlockingQueue<>(16);
-    private ArrayList<PlaneAttachment> mTouches = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSurfaceView = (GLSurfaceView) findViewById(R.id.surfaceview);
+        mSurfaceView = findViewById(R.id.surfaceview);
 
-        mSession = new Session(/*context=*/this);
+        mSession = new Session(this);
+        scene = new Scene(this, mSurfaceView, mSession, drawCallback);
 
         // Create default config, check is supported, create session from that config.
         mDefaultConfig = Config.createDefaultConfig();
@@ -107,13 +93,6 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
 
         // Set up tap listener.
         mSurfaceView.setOnTouchListener(tapListener);
-
-        // Set up renderer.
-        mSurfaceView.setPreserveEGLContextOnPause(true);
-        mSurfaceView.setEGLContextClientVersion(2);
-        mSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0); // Alpha used for plane blending.
-        mSurfaceView.setRenderer(this);
-        mSurfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
     }
 
     @Override
@@ -126,7 +105,7 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
             showLoadingMessage();
             // Note that order matters - see the note in onPause(), the reverse applies here.
             mSession.resume(mDefaultConfig);
-            mSurfaceView.onResume();
+            scene.bind();
         } else {
             CameraPermissionHelper.requestCameraPermission(this);
         }
@@ -138,7 +117,7 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
         // Note that the order matters - GLSurfaceView is paused first so that it does not try
         // to query the session. If Session is paused before GLSurfaceView, GLSurfaceView may
         // still call mSession.update() and get a SessionPausedException.
-        mSurfaceView.onPause();
+        scene.unbind();
         mSession.pause();
     }
 
@@ -167,118 +146,7 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
         }
     }
 
-    @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-
-        // Create the texture and pass it to ARCore session to be filled during update().
-        mCameraFeedRenderer.createOnGlThread(/*context=*/this);
-        mSession.setCameraTextureName(mCameraFeedRenderer.getTextureId());
-
-        // Prepare the other rendering objects.
-        try {
-            mVirtualObject.createOnGlThread(/*context=*/this, "andy.obj", "andy.png");
-            mVirtualObject.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
-
-            mVirtualObjectShadow.createOnGlThread(/*context=*/this,
-                "andy_shadow.obj", "andy_shadow.png");
-            mVirtualObjectShadow.setBlendMode(BlendMode.Shadow);
-            mVirtualObjectShadow.setMaterialProperties(1.0f, 0.0f, 0.0f, 1.0f);
-        } catch (IOException e) {
-            Log.e(TAG, "Failed to read obj file");
-        }
-        try {
-            mPlaneRenderer.createOnGlThread(/*context=*/this, "trigrid.png");
-        } catch (IOException e) {
-            Log.e(TAG, "Failed to read plane texture");
-        }
-        mPointCloud.createOnGlThread(/*context=*/this);
-    }
-
-    @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
-        GLES20.glViewport(0, 0, width, height);
-        // Notify ARCore session that the view size changed so that the perspective matrix and
-        // the video background can be properly adjusted.
-        mSession.setDisplayGeometry(width, height);
-    }
-
-    @Override
-    public void onDrawFrame(GL10 gl) {
-        // Clear screen to notify driver it should not load any pixels from previous frame.
-        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
-
-        try {
-            // Obtain the current frame from ARSession. When the configuration is set to
-            // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
-            // camera framerate.
-            Frame frame = mSession.update();
-
-            handleTap(frame);
-
-
-            // Draw background.
-            mCameraFeedRenderer.draw(frame);
-
-            // If not tracking, don't draw 3d objects.
-            if (frame.getTrackingState() == TrackingState.NOT_TRACKING) {
-                return;
-            }
-
-            // Get projection matrix.
-            float[] projmtx = new float[16];
-            mSession.getProjectionMatrix(projmtx, 0, 0.1f, 100.0f);
-
-            // Get camera matrix and draw.
-            float[] viewmtx = new float[16];
-            frame.getViewMatrix(viewmtx, 0);
-
-            // Compute lighting from average intensity of the image.
-            final float lightIntensity = frame.getLightEstimate().getPixelIntensity();
-
-            // Visualize tracked points.
-            mPointCloud.update(frame.getPointCloud());
-            mPointCloud.draw(frame.getPointCloudPose(), viewmtx, projmtx);
-
-            // Check if we detected at least one plane. If so, hide the loading message.
-            if (mLoadingMessageSnackbar != null) {
-                for (Plane plane : mSession.getAllPlanes()) {
-                    if (plane.getType() == com.google.ar.core.Plane.Type.HORIZONTAL_UPWARD_FACING &&
-                            plane.getTrackingState() == Plane.TrackingState.TRACKING) {
-                        hideLoadingMessage();
-                        break;
-                    }
-                }
-            }
-
-            // Visualize planes.
-            mPlaneRenderer.drawPlanes(mSession.getAllPlanes(), frame.getPose(), projmtx);
-
-            // Visualize anchors created by touch.
-            float scaleFactor = 1.0f;
-            for (PlaneAttachment planeAttachment : mTouches) {
-                if (!planeAttachment.isTracking()) {
-                    continue;
-                }
-                // Get the current combined pose of an Anchor and Plane in world space. The Anchor
-                // and Plane poses are updated during calls to session.update() as ARCore refines
-                // its estimate of the world.
-                planeAttachment.getPose().toMatrix(mAnchorMatrix, 0);
-
-                // Update and draw the model and its shadow.
-                mVirtualObject.updateModelMatrix(mAnchorMatrix, scaleFactor);
-                mVirtualObjectShadow.updateModelMatrix(mAnchorMatrix, scaleFactor);
-                mVirtualObject.draw(viewmtx, projmtx, lightIntensity);
-                mVirtualObjectShadow.draw(viewmtx, projmtx, lightIntensity);
-            }
-
-        } catch (Throwable t) {
-            // Avoid crashing the application due to unhandled exceptions.
-            Log.e(TAG, "Exception on the OpenGL thread", t);
-        }
-    }
-
-    private void handleTap(Frame frame) throws CameraException, NotTrackingException {
+    private void handleTap(Frame frame){
         // Handle taps. Handling only one tap per frame, as taps are usually low frequency
         // compared to frame rate.
         MotionEvent tap = mQueuedSingleTaps.poll();
@@ -286,18 +154,7 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
             for (HitResult hit : frame.hitTest(tap)) {
                 // Check if any plane was hit, and if it was hit inside the plane polygon.
                 if (hit instanceof PlaneHitResult && ((PlaneHitResult) hit).isHitInPolygon()) {
-                    // Cap the number of objects created. This avoids overloading both the
-                    // rendering system and ARCore.
-                    if (mTouches.size() >= 16) {
-                        mSession.removeAnchors(Arrays.asList(mTouches.get(0).getAnchor()));
-                        mTouches.remove(0);
-                    }
-                    // Adding an Anchor tells ARCore that it should track this position in
-                    // space. This anchor will be used in PlaneAttachment to place the 3d model
-                    // in the correct position relative both to the world and to the plane.
-                    mTouches.add(new PlaneAttachment(
-                        ((PlaneHitResult) hit).getPlane(),
-                        mSession.addAnchor(hit.getHitPose())));
+                    scene.addTouch(hit);
 
                     // Hits are sorted by depth. Consider only closest hit on a plane.
                     break;
@@ -323,8 +180,10 @@ public class HelloArActivity extends AppCompatActivity implements GLSurfaceView.
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                mLoadingMessageSnackbar.dismiss();
-                mLoadingMessageSnackbar = null;
+                if (mLoadingMessageSnackbar!= null) {
+                    mLoadingMessageSnackbar.dismiss();
+                    mLoadingMessageSnackbar = null;
+                }
             }
         });
     }
---
+++ samples/java_arcore_hello_ar/app/src/main/java/com/google/ar/core/examples/java/helloar/rendering/Scene.java
@@ -0,0 +1,202 @@
+package com.google.ar.core.examples.java.helloar.rendering;
+
+
+import android.content.Context;
+import android.opengl.GLES20;
+import android.opengl.GLSurfaceView;
+import android.util.Log;
+
+import com.google.ar.core.Frame;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.core.PlaneHitResult;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.CameraException;
+import com.google.ar.core.exceptions.NotTrackingException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+public class Scene implements GLSurfaceView.Renderer {
+  private static final String TAG = Scene.class.getSimpleName();
+
+  public interface DrawingCallback {
+    void onDraw(Frame frame);
+
+    void trackingPlane();
+  }
+
+  private CameraFeedRenderer mCameraFeedRenderer = new CameraFeedRenderer();
+  private ObjectRenderer mVirtualObject = new ObjectRenderer();
+  private ObjectRenderer mVirtualObjectShadow = new ObjectRenderer();
+  private PlaneRenderer mPlaneRenderer = new PlaneRenderer();
+  private PointCloudRenderer mPointCloud = new PointCloudRenderer();
+
+  // Temporary matrix allocated here to reduce number of allocations for each frame.
+  private final float[] mAnchorMatrix = new float[16];
+
+  private Context context;
+  private GLSurfaceView surfaceView;
+  private Session session;
+  private DrawingCallback callback;
+
+  private ArrayList<PlaneAttachment> mTouches = new ArrayList<>();
+
+  public Scene(Context context, GLSurfaceView surfaceView, Session session, DrawingCallback callback) {
+    // Set up renderer.
+    this.context = context;
+    surfaceView.setPreserveEGLContextOnPause(true);
+    surfaceView.setEGLContextClientVersion(2);
+    surfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0); // Alpha used for plane blending.
+    surfaceView.setRenderer(this);
+    surfaceView.setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);
+    this.surfaceView = surfaceView;
+
+    this.session = session;
+    this.callback = callback;
+  }
+
+  public void bind() {
+    surfaceView.onResume();
+  }
+
+  public void unbind() {
+    surfaceView.onPause();
+  }
+
+  @Override
+  public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+    GLES20.glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+
+    // Create the texture and pass it to ARCore session to be filled during update().
+    mCameraFeedRenderer.createOnGlThread(context);
+    session.setCameraTextureName(mCameraFeedRenderer.getTextureId());
+
+    // Prepare the other rendering objects.
+    try {
+      mVirtualObject.createOnGlThread(context, "andy.obj", "andy.png");
+      mVirtualObject.setMaterialProperties(0.0f, 3.5f, 1.0f, 6.0f);
+
+      mVirtualObjectShadow.createOnGlThread(context,
+          "andy_shadow.obj", "andy_shadow.png");
+      mVirtualObjectShadow.setBlendMode(ObjectRenderer.BlendMode.Shadow);
+      mVirtualObjectShadow.setMaterialProperties(1.0f, 0.0f, 0.0f, 1.0f);
+    } catch (IOException e) {
+      Log.e(TAG, "Failed to read obj file");
+    }
+
+    try {
+      mPlaneRenderer.createOnGlThread(context, "trigrid.png");
+    } catch (IOException e) {
+      Log.e(TAG, "Failed to read plane texture");
+    }
+    mPointCloud.createOnGlThread(context);
+  }
+
+  @Override
+  public void onSurfaceChanged(GL10 gl, int width, int height) {
+    GLES20.glViewport(0, 0, width, height);
+    // Notify ARCore session that the view size changed so that the perspective matrix and
+    // the video background can be properly adjusted.
+    session.setDisplayGeometry(width, height);
+  }
+
+  @Override
+  public void onDrawFrame(GL10 gl) {
+    // Clear screen to notify driver it should not load any pixels from previous frame.
+    GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT | GLES20.GL_DEPTH_BUFFER_BIT);
+
+    // Obtain the current frame from ARSession. When the configuration is set to
+    // UpdateMode.BLOCKING (it is by default), this will throttle the rendering to the
+    // camera framerate.
+    Frame frame = null;
+    try {
+      frame = session.update();
+    } catch (CameraException e) {
+      Log.e(TAG, "Could not update the session.", e);
+      return;
+    }
+
+    if (callback != null) {
+      callback.onDraw(frame);
+    }
+
+    // Draw background.
+    mCameraFeedRenderer.draw(frame);
+
+    // If not tracking, don't draw 3d objects.
+    if (frame.getTrackingState() == Frame.TrackingState.NOT_TRACKING) {
+      return;
+    }
+
+    // Get projection matrix.
+    float[] projmtx = new float[16];
+    session.getProjectionMatrix(projmtx, 0, 0.1f, 100.0f);
+
+    // Get camera matrix and draw.
+    float[] viewmtx = new float[16];
+    frame.getViewMatrix(viewmtx, 0);
+
+    // Compute lighting from average intensity of the image.
+    final float lightIntensity = frame.getLightEstimate().getPixelIntensity();
+
+    // Visualize tracked points.
+    mPointCloud.update(frame.getPointCloud());
+    mPointCloud.draw(frame.getPointCloudPose(), viewmtx, projmtx);
+
+    // Check if we detected at least one plane. If so, hide the loading message.
+    if (callback != null) {
+      for (Plane plane : session.getAllPlanes()) {
+        if (plane.getType() == com.google.ar.core.Plane.Type.HORIZONTAL_UPWARD_FACING &&
+            plane.getTrackingState() == Plane.TrackingState.TRACKING) {
+          callback.trackingPlane();
+          break;
+        }
+      }
+    }
+
+    // Visualize planes.
+    mPlaneRenderer.drawPlanes(session.getAllPlanes(), frame.getPose(), projmtx);
+
+    // Visualize anchors created by touch.
+    float scaleFactor = 1.0f;
+    for (PlaneAttachment planeAttachment : mTouches) {
+      if (!planeAttachment.isTracking()) {
+        continue;
+      }
+      // Get the current combined pose of an Anchor and Plane in world space. The Anchor
+      // and Plane poses are updated during calls to session.update() as ARCore refines
+      // its estimate of the world.
+      planeAttachment.getPose().toMatrix(mAnchorMatrix, 0);
+
+      // Update and draw the model and its shadow.
+      mVirtualObject.updateModelMatrix(mAnchorMatrix, scaleFactor);
+      mVirtualObjectShadow.updateModelMatrix(mAnchorMatrix, scaleFactor);
+      mVirtualObject.draw(viewmtx, projmtx, lightIntensity);
+      mVirtualObjectShadow.draw(viewmtx, projmtx, lightIntensity);
+    }
+  }
+
+  public void addTouch(HitResult hit) {
+    // Cap the number of objects created. This avoids overloading both the
+    // rendering system and ARCore.
+    if (mTouches.size() >= 16) {
+      session.removeAnchors(Arrays.asList(mTouches.get(0).getAnchor()));
+      mTouches.remove(0);
+    }
+    // Adding an Anchor tells ARCore that it should track this position in
+    // space. This anchor will be used in PlaneAttachment to place the 3d model
+    // in the correct position relative both to the world and to the plane.
+    try {
+      mTouches.add(new PlaneAttachment(
+          ((PlaneHitResult) hit).getPlane(),
+          session.addAnchor(hit.getHitPose())));
+    } catch (NotTrackingException e) {
+      Log.e(TAG, "Session is not tracking.");
+    }
+  }
+}
