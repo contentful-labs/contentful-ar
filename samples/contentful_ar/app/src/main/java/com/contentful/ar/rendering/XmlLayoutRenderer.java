29b75d1c75d9971cbcdf15f1ee94135c75237fef
Mario Bodemann <mario@contentful.com>
2018-03-05 11:40:15 +0100

    Rename project from pirates to contentful

---
+++ samples/contentful_ar/app/src/main/java/com/contentful/ar/rendering/XmlLayoutRenderer.java
@@ -0,0 +1,60 @@
+package com.contentful.ar.rendering;
+
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.hardware.display.DisplayManager;
+import android.support.annotation.LayoutRes;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.io.FileNotFoundException;
+
+import static com.contentful.ar.rendering.ShaderUtil.normalizeFileName;
+
+public class XmlLayoutRenderer extends ObjectRenderer {
+  private Bitmap bitmap;
+
+  public XmlLayoutRenderer(Context context, @LayoutRes int xmlLayoutResource) {
+    super(
+        normalizeFileName("plane.obj", basepath(context)),
+        "",
+        normalizeFileName(ObjectRendererFactory.DEFAULT_FRAGMENT_SHADER_FILE_NAME, basepath(context)),
+        normalizeFileName(ObjectRendererFactory.DEFAULT_VERTEX_SHADER_FILE_NAME, basepath(context)));
+
+    loadTexture(context, xmlLayoutResource);
+
+    setBlendMode(BlendMode.Grid);
+  }
+
+  private static String basepath(Context context) {
+    return context.getExternalFilesDir(null).getAbsolutePath();
+  }
+
+  @Override protected Bitmap readTexture() throws FileNotFoundException {
+    return bitmap;
+  }
+
+  private void loadTexture(Context context, @LayoutRes int layout) {
+    final DisplayMetrics displayMetrics = new DisplayMetrics();
+    final DisplayManager manager = context.getSystemService(DisplayManager.class);
+    manager.getDisplays()[0].getMetrics(displayMetrics);
+    final int height = displayMetrics.heightPixels;
+    final int width = displayMetrics.widthPixels;
+
+    bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+    final Canvas canvas = new Canvas(bitmap);
+
+    int measuredWidth = View.MeasureSpec.makeMeasureSpec(width, View.MeasureSpec.EXACTLY);
+    int measuredHeight = View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY);
+
+    LayoutInflater inflater = LayoutInflater.from(context);
+    View view = inflater.inflate(layout, null, false);
+
+    view.measure(measuredWidth, measuredHeight);
+    view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+    view.draw(canvas);
+  }
+}
